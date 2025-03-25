e74b5e68c1cfa620484796a5c2f53667f37f92ee
hpeacher <hpeacher@iastate.edu>
2025-03-25 01:07:37 -0500

    The app is using expo router to route from the Welcome Screen to the Login Screen.

---
+++ UniPark/app/Screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function LoginScreen(props) {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default LoginScreen;
